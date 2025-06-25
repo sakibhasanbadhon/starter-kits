f983c5162e4f79b25609d6b1344b071898f41f53
sujon <sujonbdjoin019@gmail.com>
2025-06-24 20:58:07 -0700

    update asset method

--- resources/views/admin/includes/side-nav.blade.php
+++ resources/views/admin/includes/side-nav.blade.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="index3.html" class="brand-link">
-          <img src="{{ asset('public/backend') }}/img/AdminLTELogo.png" alt="AdminLTE Logo"
+          <img src="{{ asset('backend') }}/img/AdminLTELogo.png" alt="AdminLTE Logo"
               class="brand-image img-circle elevation-3" style="opacity: .8">
           <span class="brand-text font-weight-light">Starter Kits</span>
       </a>
--- resources/views/admin/partials/script.blade.php
+++ resources/views/admin/partials/script.blade.php
@@ -1,11 +1,11 @@
 <!-- jQuery -->
-<script src="{{ asset('public/backend/') }}/js/jquery.min.js"></script>
+<script src="{{ asset('backend/') }}/js/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('public/backend') }}/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('backend') }}/js/bootstrap.bundle.min.js"></script>
 <!-- Seelct 2 -->
-<script src="{{ asset('public/backend/js/select2.min.js') }}"></script>
+<script src="{{ asset('backend/js/select2.min.js') }}"></script>
 <!-- spartan multi image picker -->
-<script src="{{ asset('public/backend/js/spartan-multi-image-picker-min.js') }}"></script>
+<script src="{{ asset('backend/js/spartan-multi-image-picker-min.js') }}"></script>
 
 
 <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
@@ -20,8 +20,8 @@
 <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.colVis.min.js"></script>
 
 <!-- AdminLTE App -->
-<script src="{{ asset('public/backend') }}/js/adminlte.min.js"></script>
-<script src="{{ asset('public/backend') }}/js/main.js"></script>
+<script src="{{ asset('backend') }}/js/adminlte.min.js"></script>
+<script src="{{ asset('backend') }}/js/main.js"></script>
 <script>
     $(document).ready(function () {
         $(document).on('click', '.logout-btn', function(){
